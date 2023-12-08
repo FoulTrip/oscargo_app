5966d92a9de00662985ba0a45724113cf1062cd4
foultrip <davidvasquezmahecha@gmail.com>
2023-12-08 11:52:41 -0500

    act001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,149 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import TypeWriter from "typewriter-effect";
+import modelCargo from "@/assets/modelCargo.jpeg";
+
+import { IoIosArrowRoundBack } from "react-icons/io";
+
+import modelLogo from "@/assets/oscargo_logo.png";
+import MiniCargo from "@/components/services/miniCargo";
+import MedianaCargo from "@/components/services/medianaCargo";
+import LongCargo from "@/components/services/longCargo";
+import Remolque from "@/components/services/Remolque";
+
+function HomePage() {
+  const [selectOpt, setSelectOpt] = useState("");
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className={styles.heroin_section}>
+        <div className={styles.boxItems}>
+          <Image className={styles.imgCargo} src={modelCargo} alt="cargo" />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <div className={styles.boxItemsName}>
+          <p className={styles.nameCompany}>
+            <TypeWriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Oscargo")
+                  .pauseFor(2500)
+                  // .deleteAll()
+                  .start();
+              }}
+            />
           </p>
-        </a>
-      </div>
-    </main>
-  )
+          <p className={styles.productDescription}>
+            ¡Descubre la libertad de viajar y transportar con estilo! En
+            Oscargo, te ofrecemos nuestras increíbles bicicletas de carga,
+            perfectas para aventuras en la ciudad y escapadas con carga.
+            Diseñadas para la comodidad y la funcionalidad, nuestras bicicletas
+            te brindarán la versatilidad que necesitas en cada viaje. ¡Haz que
+            cada recorrido sea una experiencia única con Oscargo
+          </p>
+          {/* <div className={styles.btnVisit}>
+            <div className={styles.realBtn}>Explorar</div>
+          </div> */}
+        </div>
+      </main>
+
+      <main className={styles.secondMain}>
+        <div className={styles.barTitles}>
+          <div className={styles.centerItemBar}>
+            <div className={styles.boxTexts}>
+              <h1 className={styles.titleBartop}>NUESTRA PEDALOGIA</h1>
+              <p className={styles.subText}>Hormigas sin fronteras</p>
+            </div>
+            <div className={styles.boxImageLogo}>
+              <Image
+                className={styles.iconLogoTest}
+                src={modelLogo}
+                alt="logo"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.BoxInfo}>
+          <div className={styles.SubBoxInfo}>
+            <div className={styles.subCard}>
+              <h3>Mision</h3>
+              <p>
+                Facilitar la libertad de moverte con elegancia y practicidad. En
+                Oscargo, creamos bicicletas de carga excepcionales, ideales para
+                aventuras urbanas y escapadas con carga. Nuestro compromiso es
+                diseñar bicicletas que ofrezcan comodidad y funcionalidad,
+                brindándote la versatilidad necesaria en cada viaje. Transforma
+                cada recorrido en una experiencia única con Oscargo.
+              </p>
+            </div>
+          </div>
+          <div className={styles.SubBoxInfo}>
+            <div className={styles.subCard}>
+              <h3>Vision</h3>
+              <p>
+                Visualizamos un mundo donde la movilidad sostenible es sinónimo
+                de estilo. En Oscargo, aspiramos a ser líderes en proporcionar
+                bicicletas de carga innovadoras que se integren perfectamente en
+                la vida diaria. Nos esforzamos por ser reconocidos por nuestra
+                calidad, versatilidad y el toque único que añadimos a cada
+                viaje. Con Oscargo, el transporte y la aventura se fusionan para
+                crear un futuro más emocionante y sostenible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <main className={styles.mainServices}>
+        {selectOpt == "" ? (
+          <h1 className={styles.serviceTitle}>Servicios</h1>
+        ) : (
+          <div className={styles.barBtnBack} onClick={() => setSelectOpt("")}>
+            <div className={styles.btnBack}>
+              <div className={styles.boxIconBack}>
+                <IoIosArrowRoundBack />
+              </div>
+              <p>Servicios</p>
+            </div>
+          </div>
+        )}
+
+        {selectOpt == "" ? (
+          <div className={styles.boxCard}>
+            <div
+              className={styles.subBoxCard}
+              onClick={() => setSelectOpt("miniCargo")}
+            >
+              <h1 className={styles.titleService}>Cargo Mini</h1>
+            </div>
+            <div
+              className={styles.subBoxCard}
+              onClick={() => setSelectOpt("medianaCargo")}
+            >
+              <h1 className={styles.titleService}>Cargo Mediana</h1>
+            </div>
+            <div
+              className={styles.subBoxCard}
+              onClick={() => setSelectOpt("longCargo")}
+            >
+              <h1 className={styles.titleService}>Cargo longjhon</h1>
+            </div>
+            <div
+              className={styles.subBoxCard}
+              onClick={() => setSelectOpt("remolque")}
+            >
+              <h1 className={styles.titleService}>Remolque</h1>
+            </div>
+          </div>
+        ) : null}
+        {selectOpt == "miniCargo" ? <MiniCargo /> : null}
+        {selectOpt == "medianaCargo" ? <MedianaCargo /> : null}
+        {selectOpt == "longCargo" ? <LongCargo /> : null}
+        {selectOpt == "remolque" ? <Remolque /> : null}
+      </main>
+    </>
+  );
 }
+
+export default HomePage;
---
+++ src/components/services/longCargo.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Image from "next/image";
+
+import modelCargoMini from "@/assets/modelo_mini_cargo.webp";
+
+function LongCargo() {
+  return (
+    <>
+      <div className={styles.boxCards}>
+        <h1 className={styles.nameService}>Cargo Grande</h1>
+        <div className={styles.card}>
+          <div className={styles.subCard}>
+            <div className={styles.boxImage}>
+              <Image
+                className={styles.bicImage}
+                src={modelCargoMini}
+                alt="mini"
+              />
+            </div>
+            <p className={styles.nameroduct}>Cargo Mini v1</p>
+            <p className={styles.priceProduct}>Precio: $4'000.000</p>
+          </div>
+
+          <div className={styles.subCard}>
+            <div className={styles.boxImage}>
+              <Image
+                className={styles.bicImage}
+                src={modelCargoMini}
+                alt="mini"
+              />
+            </div>
+            <p className={styles.nameroduct}>Cargo Mini v1</p>
+            <p className={styles.priceProduct}>Precio: $4'000.000</p>
+          </div>
+
+          <div className={styles.subCard}>
+            <div className={styles.boxImage}>
+              <Image
+                className={styles.bicImage}
+                src={modelCargoMini}
+                alt="mini"
+              />
+            </div>
+            <p className={styles.nameroduct}>Cargo Mini v1</p>
+            <p className={styles.priceProduct}>Precio: $4'000.000</p>
+          </div>
+
+          <div className={styles.subCard}>
+            <div className={styles.boxImage}>
+              <Image
+                className={styles.bicImage}
+                src={modelCargoMini}
+                alt="mini"
+              />
+            </div>
+            <p className={styles.nameroduct}>Cargo Mini v1</p>
+            <p className={styles.priceProduct}>Precio: $4'000.000</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LongCargo;
